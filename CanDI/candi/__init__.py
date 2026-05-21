54c13ee461156e2e24d6799be69972289a737467
Abe Arab <abarbiology@gmail.com>
2026-05-21 15:10:26 -0700

    del

--- CanDI/candi/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import data
-
-data = data.Data() #Global object data instantiated on import required for access by GeneQuery Objects
-
-from .candi import (Gene, CellLine, Organelle, Cancer, CellLineCluster, GeneCluster)
